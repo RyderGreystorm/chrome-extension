742ed48209488e138fe8d09716dd18504587eb73
Godbless <biekrogodbless@gmail.com>
2022-09-27 11:29:41 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let myLead = [];
+const inputEl = document.getElementById('input-el')
+const inputBtn = document.getElementById('input-btn')
+
+inputBtn.addEventListener('click', function () {
+   myLead.push('www.awesomelead.com')
+   console.log(myLead)
+})
+
+
+ 
